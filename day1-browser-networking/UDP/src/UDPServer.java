1976c2491d0c9ead654a1d881172861820271422
Aleksej Otjan <Aleksej Otjan>
2016-01-08 23:40:40 +0200

    Add multithread UDPServer

--- day1-browser-networking/UDP/src/UDPServer.java
+++ day1-browser-networking/UDP/src/UDPServer.java
@@ -8,38 +8,52 @@ import java.util.logging.Logger;
 /**
  * Simple UDP Server
  */
-public class UDPServer {
+public class UDPServer implements Runnable {
     private final static int PACKET_SIZE = 256;
     private static Logger log = Logger.getLogger(UDPServer.class.getName());
 
-    public static void main(String[] args) throws IOException{
-        if(args.length != 1){
+    private DatagramSocket socket = null;
+    UDPServer(DatagramSocket socket) {
+        this.socket = socket;
+    }
+
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = null;
+        if (args.length != 1) {
             System.out.println("usage: java UDPServer port");
             return;
         }
         try {
             int port = Integer.parseInt(args[0]);
-            DatagramSocket socket = new DatagramSocket(port);
+            socket = new DatagramSocket(port);
             System.out.println("The server has started....");
             while (true) {
-                byte[] receiveData = new byte[PACKET_SIZE];
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                socket.receive(receivePacket);
-                displayInfo(receivePacket);
-                socket.send(receivePacket);
-
-                log.info("Close connection");
+                new Thread(new UDPServer(socket)).start();
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
 
-    public static void displayInfo(DatagramPacket packet){
+    public static void displayInfo(DatagramPacket packet) {
         System.out.println("Received: ");
         System.out.println("IP Address: " + packet.getAddress());
         System.out.println("Port: " + packet.getPort());
         System.out.println("Date: " + new Date());
-        System.out.println("Message: "+ new String(packet.getData()));
+        System.out.println("Message: " + new String(packet.getData()));
+    }
+
+    public void run() {
+        byte[] receiveData = new byte[PACKET_SIZE];
+        try {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            socket.receive(receivePacket);
+            displayInfo(receivePacket);
+            socket.send(receivePacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        log.info("Close connection");
     }
 }
